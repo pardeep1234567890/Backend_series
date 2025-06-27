259c2e93dc12f3b3418d2426507cbc8827ac15b0
pardeep1234567890 <pardeep905001@gmail.com>
2025-06-27 11:43:35 +0530

    add refresh token functionality and update user routes

--- backend_s1/src/controllers/user.controller.js
+++ backend_s1/src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { apiError } from "../utils/apiError.js";
 import {User} from "../models/user.model.js";
 import {uploadOnCloudinary} from "../utils/cloudinary.js";
 import {apiResponse} from "../utils/apiResponse.js";
+import jwt from "jsonwebtoken"
 
 const registerUser = asyncHandler(async (req,res)=>{    //It takes a function as input (requestHandler) // That input is your actual async route handler
             // get user details from frontend 
@@ -128,8 +129,8 @@ const loginUser = asyncHandler(async(req,res)=>{
     // send cookie to give tokens
     return res
     .status(200)
-    .cookie("accessToken",accessToken)
-    .cookie("refreshToken",refreshToken)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
     .json(
         new apiResponse(
             200,
@@ -167,4 +168,44 @@ const logoutUser = asyncHandler(async (req,res)=>{
     .json(new apiResponse(200,{},"User logged out"))
 
 })
-export {registerUser,loginUser,logoutUser} 
+
+// Here we refresh the token
+const refreshAccessToken = asyncHandler(async (req,res)=>{
+    const incomingRefreshToken = req.cookie.refreshToken || req.body.refreshToken
+
+    if (!incomingRefreshToken) {
+        throw new apiError(401,"Unauthorized Request")
+    }
+    try {
+        const decodedToken = jwt.verify(incomingRefreshToken,process.env.REFRESH_TOKEN_SECRET)
+    
+        const user = User.findById(decodedToken?._id)                 //? = Safely access properties that might not exist       
+        if (!user) {
+            throw new apiError(401,"Invailid Refresh Token")
+        }
+        if (incomingRefreshToken !== user?.refreshToken) {
+            throw new apiError(401,"Refresh Token is Expired or used ")
+        }
+        const options ={
+            httpOnly:true,
+            secure : true
+        }
+        const{accessToken,newrefreshToken} =await generateAccessTokenAndRefreshTokens(user._id)
+    
+        return res
+        .status(200)
+        .cookie("accessToken",accessToken,options)
+        .cookie("refeshToken",newrefreshToken,options)
+        .json(
+            200,
+            {
+                accessToken,refreshToken : newrefreshToken
+            },
+            "Access Token Refreshed"
+        )
+    } catch (error) {
+        throw new apiError(401,"Invalid Refresh Token")
+    }
+})
+
+export {registerUser,loginUser,logoutUser,refreshAccessToken} 
